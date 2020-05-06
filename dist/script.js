1043b4a2539e72d16b9b399e662de7064add2b84
Robert Zahn <rob.jzahn@gmail.com>
2020-05-06 16:30:51 -0400

    Add seat selector event listener

--- dist/script.js
+++ dist/script.js
@@ -5,3 +5,12 @@ const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
 const ticketPrice = +movieSelect.value; //+ turns a string into a number
+
+container.addEventListener('click', e => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected');
+  }
+});
